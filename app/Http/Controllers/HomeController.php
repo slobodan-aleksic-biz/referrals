0775e5720f53d54815f3a5b2bfa3331a2077fab3
Slobodan Aleksic <slobodan.aleksic.biz@gmail.com>
2021-06-16 17:24:40 +0200

    0006 add show methos to see referrences of a choosen user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,16 +25,62 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $allUsers = User::query()
-            ->select([
+        $user = auth()->user();
+
+        [
+            $numOfMyReferrals,
+            $allUsers,
+        ] = $this->defaultData($user);
+
+        return view('home', [
+            'numOfMyReferrals' => $numOfMyReferrals,
+            'allUsers' => $allUsers,
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        $numOfMyReferrals = User::numOfMyReferrals($user)->count();
+
+        [
+            $numOfMyReferrals,
+            $allUsers,
+        ] = $this->defaultData($user, false);
+
+        return view('home', [
+            'numOfMyReferrals' => $numOfMyReferrals,
+            'allUsers' => $allUsers,
+            'user' => $user,
+        ]);
+    }
+
+    private function defaultData($user, $isIndex = true)
+    {
+        $numOfMyReferrals = User::numOfMyReferrals($user)->count();
+
+        if($isIndex) {
+            $allUsers = User::where('id', '!=', $user->id);
+        } else {
+            $allUsers = User::where('referrer_id', '=', $user->id);
+        }
+
+        $allUsers = $allUsers->select([
                 'users.*',
                 DB::raw('(select count(*) from users u where u.referrer_id = users.id) as cnt')
             ])
             ->orderBy('cnt', 'desc')
             ->paginate(3);
 
-        return view('home', [
-            'allUsers' => $allUsers
-        ]);
+        return [
+            $numOfMyReferrals,
+            $allUsers
+        ];
     }
 }
